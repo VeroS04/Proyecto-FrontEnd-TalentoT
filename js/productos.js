6c7498d89a86a0f94f539ae195fd7806f4f8127d
Veronica <veronica.c.segovia@gmail.com>
2025-12-04 09:31:12 -0300

    Se crearon las tarjetas de productos dede js en el archivo index.js con el boton y la funcion/evento de agregar al carrito

--- js/productos.js
+++ js/productos.js
@@ -1,6 +1,6 @@
 export const productos = [
-  { id: "1", nombre: "Figura-Luffy", img: "img/figura-luffy.webp", precio: 0 },
-  { id: "2", nombre: "Figura-Zoro-HG", img: "img/ZOROEGG.webp", precio: 20 },
+  { id: "1", nombre: "Figura-Luffy", img: "img/figura-luffy.webp", precio: 20 },
+  { id: "2", nombre: "Figura-Zoro-EGG", img: "img/ZOROEGG.webp", precio: 20 },
   { id: "3", nombre: "Mate-POCHITA", img: "img/mate-pochita.jpg", precio: 20 },
   { id: "4", nombre: "Media-TOTORO", img: "img/medias-totoro.webp", precio: 20 },
   { id: "5", nombre: "Cosplay-SHINIBU-KOCHO", img: "img/cosplay-shinobu.jpg", precio: 40 },
